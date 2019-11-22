10ee18bea38dcaaee0d17681c86e754d0e21c682
Rangel Rangelov <rangel@rubberduck.xyz>
2019-11-22 14:29:41 +0200

    Initial commit setting base and implementing db connection

---
+++ index.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+dotenv.config();
+
+const app = express();
+
+app.listen(process.env.PORT, () => { console.log(`Server is running on ${process.env.PORT} port.`)});
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+if(!db) {
+  console.log(chalk.bgRed('Error connecting to db '))
+} else {
+  console.log(chalk.bgGreen('Success connecting to db'))
+}
